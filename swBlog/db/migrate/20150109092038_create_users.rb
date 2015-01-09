c089e0a422110d3bddfd8a05781175cfc70e740e
wangsen <wangs@tongxinyiliao.com>
2015-01-09 17:47:54 +0800

    添加user的model和controller

---
+++ swBlog/db/migrate/20150109092038_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :nickname, limit: 30
+      t.string :dodo_number, limit: 13,null: false
+      t.string :email, limit: 25,null: false
+      t.string :token, limit: 45,null: false
+      t.string :account_state, limit: 10,null: false, default: '正常'
+
+      t.timestamps null: false
+    end
+  end
+end
