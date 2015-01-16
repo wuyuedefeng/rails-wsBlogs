888e0115b74c6ad6d9f6237c08156fb53c0585f1
wangsen <wangs@tongxinyiliao.com>
2015-01-16 14:58:35 +0800

    添加gem annotate(备注)

--- swBlog/app/models/user.rb
+++ swBlog/app/models/user.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :integer          not null, primary key
+#  nickname      :string(30)
+#  dodo_number   :string(13)       not null
+#  email         :string(25)       not null
+#  token         :string(45)       not null
+#  account_state :string(10)       default("正常"), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class User < ActiveRecord::Base
 end
--- swBlog/test/models/user_test.rb
+++ swBlog/test/models/user_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :integer          not null, primary key
+#  nickname      :string(30)
+#  dodo_number   :string(13)       not null
+#  email         :string(25)       not null
+#  token         :string(45)       not null
+#  account_state :string(10)       default("正常"), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
