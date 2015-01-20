330e1d8ce32579eba350a4d58786979889d5617d
senwang <578595193@qq.com>
2015-01-20 19:29:56 +0800

    添加后台分组

--- swBlog/app/controllers/admin/categories_controller.rb
+++ swBlog/app/controllers/admin/categories_controller.rb
@@ -4,10 +4,23 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
+  	@category = Category.new
   end
 
   def create
-  	category = Category.new(params[:category])
-  	category.save
+  	p '----------'
+  	p params
+  	category = Category.new(params[:category].symbolize_keys)
+  	if category.save
+  		redirect_to '/admin/categories'
+  	 else 	
+  	 	render :action => :new
+  	 end  
+  end
+
+  def destroy
+  	category = Category.find(params[:id])
+    category.destroy
+    redirect_to action: :index
   end
 end
