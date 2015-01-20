2728b23f7d86a01dc7cd270c73476b78f9c12066
wangsen <wangs@tongxinyiliao.com>
2015-01-20 17:31:23 +0800

    新增博客

--- swBlog/app/controllers/blogs_controller.rb
+++ swBlog/app/controllers/blogs_controller.rb
@@ -13,8 +13,18 @@ class BlogsController < ApplicationController
   end
 
   def create
-  	p '----------------==========1==create_blog'
-    p params
-    render :action => :index
+  	blog = Blog.new(blog_params)
+    if blog.save
+      flash[:notice] = "新增成功"
+      render :action => :index
+    else
+      render json: {txt: "新增失败"} 
+    end
+    
+  end
+
+  private 
+  def blog_params
+    params.require(:blog).permit(:title, :tags, :body, :category_id)
   end
 end
