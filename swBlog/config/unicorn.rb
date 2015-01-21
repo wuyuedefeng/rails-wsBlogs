f04f81b218930ec5d90635b6db8d237280f29e61
wangsen <wangs@tongxinyiliao.com>
2015-01-21 15:15:40 +0800

    deploy

--- swBlog/config/unicorn.rb
+++ swBlog/config/unicorn.rb
@@ -1,10 +1,10 @@
-worker_processes 2
+worker_processes 4
 timeout 30
 
 APP_PATH = File.expand_path("../..", __FILE__)
 working_directory APP_PATH
 
-listen 8080, :tcp_nopush => true
+listen 8080, :tcp_nopush => false
 listen "/tmp/unicorn.sock", :backlog => 64
 
 stderr_path APP_PATH + "/log/unicorn.stderr.log"
