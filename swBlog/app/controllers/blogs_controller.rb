b9e46633c7fe18a12b18722ddaab0a5a78249795
wangsen <wangs@tongxinyiliao.com>
2015-01-29 20:09:54 +0800

    优化搜索

--- swBlog/app/controllers/blogs_controller.rb
+++ swBlog/app/controllers/blogs_controller.rb
@@ -2,12 +2,14 @@ class BlogsController < ApplicationController
   before_action :is_login, except: [:show, :index]
   def index
     if params[:all_blog_search_text].present?
+      query= params[:all_blog_search_text].split(' ').join('%')
       @blogs = Blog.where("title LIKE ? or tags LIKE ?",
-        "%#{params[:all_blog_search_text]}%","%#{params[:all_blog_search_text]}%").
+        "%#{query}%","%#{params[query]}%").
         order("created_at desc").page(params[:page])
     elsif params[:my_blog_search_text].present?
+      query = params[:my_blog_search_text].split(' ').join('%')
       @blogs = Blog.where("user_id = ? and (title LIKE ? or tags LIKE ?)",
-        current_user.id,"%#{params[:my_blog_search_text]}%","%#{params[:my_blog_search_text]}%").
+        current_user.id,"%#{query}%","%#{query}%").
         order("created_at desc").page(params[:page])
     else
       @blogs = current_user.blogs.order("created_at desc").page(params[:page])
